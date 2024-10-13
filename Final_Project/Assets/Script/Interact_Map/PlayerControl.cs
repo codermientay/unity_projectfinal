fc4e622859cb627609fa635c256adbd9c4601208
iuh21082861 <phamhoanglong031004@gmail.com>
2024-10-13 11:05:12 +0700

    Done town_1 and movement

--- Final_Project/Assets/Script/Interact_Map/PlayerControl.cs
+++ Final_Project/Assets/Script/Interact_Map/PlayerControl.cs
@@ -1,18 +1,37 @@
+using System.Threading;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Set animation parameters
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        // Normalize movement to avoid faster diagonal movement
+        if (movement.magnitude > 1)
+        {
+            movement = movement.normalized;
+        }
+
+        // Move player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
